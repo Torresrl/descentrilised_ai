80f6871537f0b3342a5d06c8f367a77d20d0f1de
Torres Rossland Lande <torresrl@idun-login3.foreman.hpc.ntnu.no>
2020-05-05 17:21:19 +0200

    new data

--- reinforcment_learning_bigger_box.py
+++ reinforcment_learning_bigger_box.py
@@ -480,13 +480,13 @@ def try_model(key, imgs, labels, model_info, round_nr, lower_bound=True):
 # In[10]:
 
 
-policy_lr = 1e-3
-value_lr = 1e-4 #-2
+policy_lr = 1e-2
+value_lr = 1e-3 #-2
 
 policy_clip = 3
 value_clip = 3
 
-reward_exponential = 10
+reward_exponential = 3
 reward_multi = 10
 
 #tar in state og retunere action
@@ -500,7 +500,7 @@ class policy_model:
         inputes = Input(shape=(innput_size*4,))
         actions_true = Input(shape=[innput_size], name='actions_true')
         advantages = Input(shape=[1], name='advantages')
-        _ = Dense(1024, activation=ACTI)(inputes) #512
+        _ = Dense(2048, activation=ACTI)(inputes) #512
         #_ = Dense(32, activation=ACTI)(_)
         #_ = Dropout(0.1)(_)
         #_ = Dense(1024, activation=ACTI)(_)
@@ -518,7 +518,7 @@ class policy_model:
         def custom_loss(y_true, y_pred, adv):
             log_lik =  K.log(y_true * (y_true- y_pred) + (1 - y_true) * (y_true + y_pred))
             loss = 1 / (-K.mean(log_lik * adv, keepdims=True))#removed was (-K.mean....)
-            return K.clip(loss, -20, 20)
+            return K.clip(loss, -30, 30)
             #return loss
 
         #self.policy.compile(optimizer=Adam(lr=1e-2), loss=custom_loss)
@@ -745,7 +745,7 @@ def play_one_episode(hist_key, data_generator, model_info, value_fuction, policy
 # In[11]:
 
 
-TEST_ROUNDS = 5
+TEST_ROUNDS = 10
 REDUCED_LIST = False
 gamma = 0.99
 box_size = 5 #2
@@ -848,7 +848,7 @@ model_info_save = {
     'model_info': model_info
 }
 #model_info, num_models, num_trials
-file_save_path_name = f'pg_ressults/box_5/pg_{optimizer}_{policy_lr}_val_{value_lr}_{TEST_ROUNDS}_{np.array(total_reward).mean()}.json'
+file_save_path_name = f'pg_ressults/box_5_batch_10/pg_{optimizer}_{policy_lr}_val_{value_lr}_{TEST_ROUNDS}_{np.array(total_reward).mean()}.json'
 
 with open(file_save_path_name, "w") as file_write:
     # write json data into file
